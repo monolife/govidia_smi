664fcea8c526489572e025f3d651c22f581e615c
aducao <aducao@dzynetech.com>
2020-12-07 02:26:45 -0500

    Better REST response, fix for 'omitempty' of gpu_index, different server port

--- monitor/main.go
+++ monitor/main.go
@@ -3,8 +3,7 @@ package main
 //Standard Library
 import (
 	"context"
-	// "encoding/json"
-	"fmt"
+	"encoding/json"
 	"log"
 	"net/http"
 	"os"
@@ -15,7 +14,7 @@ import (
 //Third Party
 import (
 	"gopkg.in/yaml.v2"
-	pjson "google.golang.org/protobuf/encoding/protojson"
+	
 	"google.golang.org/grpc"
 	pb "ducao/govidia_smi/proto"
 
@@ -23,7 +22,7 @@ import (
 )
 
 type Configuration struct{
-	MonitorUiPort	int			`yaml:"monitorUiPort"`// gRPC recieving port for Ingest process server
+	MonitorPort	int			`yaml:"monitorPort"`// gRPC recieving port for Ingest process server
 	AgentPort		int			`yaml:"agentPort"`// gRPC recieving port for Ingest process server
 	AgentHosts		[]string 	`yaml:"agentHosts,flow"`// gRPC recieving port for Ingest process server
 }
@@ -52,10 +51,7 @@ func QueryGpus(hostname string)(response *pb.NvidiaQueryResponse, err error){
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
-	// log.WithFields(logrus.Fields{
-	// 					"Filename": filename,
-	// 					"Id": id,
-	// 				}).Info("Calling Ingest")
+
 	response, err = client.QueryGpu(ctx, &pb.DataCue{GpuIndex: 0})
 	if err != nil {
 		log.Println(err)
@@ -64,62 +60,53 @@ func QueryGpus(hostname string)(response *pb.NvidiaQueryResponse, err error){
 	if( response.Infos[0].GpuIndex == 0){
 		response.Infos[0].GpuIndex = 0;
 	}
-	// log.Info("Ingest of ID ", r.GetId()," complete")
+
 	return;
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func setupResponse(w *http.ResponseWriter, req *http.Request) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	(*w).Header().Set("Content-Type", "application/json")
+}
 
-	log.Println(r)
-    fmt.Fprintf(w, "Hi there, %s [%v]!\n\n", r.URL.Path[1:], _count)
-    
-    for _,hostname := range _config.AgentHosts{
-	    response, err := QueryGpus(hostname)
-	    if err != nil {
+func handler(w http.ResponseWriter, r *http.Request) {
+	setupResponse(&w, r)
+	var resps []*pb.NvidiaQueryResponse
+
+	for _,hostname := range _config.AgentHosts{
+		response, err := QueryGpus(hostname)
+		if err != nil {
 			log.Println(err)
-	    	fmt.Fprintf(w, "No response from %s\n", hostname)
 			break
 		}
-
-
-		marshalOpts := pjson.MarshalOptions{
-			Multiline:true,
-			EmitUnpopulated:true,
-		}
-		jsonByte,err := marshalOpts.Marshal(response)
-		log.Println(_count)
-		log.Println(string(jsonByte))
-	    fmt.Fprintf(w, "%s\n", string(jsonByte))
-	    _count++
+		resps = append(resps, response)
 	}
+	json.NewEncoder(w).Encode(resps)
 
-	jsonByte,err := marshalOpts.Marshal(response)
-
-  fmt.Fprintf(w, "%s\n", string(jsonByte))
 }
 
 func handleOne(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+
+	setupResponse(&w, r)
+
 	params := mux.Vars(r)
+	log.Println(params)
 
 	response, err := QueryGpus(params["host"])
 	if err != nil {
 		log.Println(err)
 		json.NewEncoder(w).Encode(&pb.NvidiaQueryResponse{})
 	}else{
-
-		marshalOpts := pjson.MarshalOptions{
-			Multiline:true,
-			EmitUnpopulated:true,
-		}
-		jsonByte,err := marshalOpts.Marshal(response)
-		// fmt.Fprintf(w, "%s\n", string(jsonByte))
 		json.NewEncoder(w).Encode(response)
-
 	}
 
 }
-func doNothing(w http.ResponseWriter, r *http.Request){}
+
+func doNothing(w http.ResponseWriter, r *http.Request){
+	//NO-OP function to deal with things like favicon fetching
+}
 
 func main(){
 	/*------------------ Load config -------------------------------*/
@@ -131,7 +118,7 @@ func main(){
 	defer file.Close();
 	if err != nil {
 		log.Println("!!! Failed to open config file %v (Using hardcoded defaults) !!!", err)
-		_config.MonitorUiPort = 8080;
+		_config.MonitorPort = 8080;
 		_config.AgentHosts = []string{"localhost"};
 		_config.AgentPort = 1234;
 	} else {
@@ -148,12 +135,7 @@ func main(){
 	log.Println("===========");
 	/*------------------(end) Load config --------------------------*/
 
-<<<<<<< HEAD
-	uiPort := ":"+strconv.Itoa(_config.MonitorUiPort);
-	http.HandleFunc("/", handler);
-	http.HandleFunc("/favicon.ico", doNothing)
-    log.Println(http.ListenAndServe(uiPort, nil));
-=======
+	monitorPort := ":"+strconv.Itoa(_config.MonitorPort);
 
 	// if _,err = QueryGpus(_config.AgentHost); err != nil {
 	// 	log.Fatal(err)
@@ -161,9 +143,9 @@ func main(){
 
   router := mux.NewRouter();
 	router.HandleFunc("/", handler)
+	router.HandleFunc("/favicon.ico", doNothing)
   router.HandleFunc("/query", handler).Methods("GET");
   router.HandleFunc("/query/{host}", handleOne).Methods("GET")
 
-  log.Fatal(http.ListenAndServe(":8080", router))
->>>>>>> ef9608bbd8162b8654d34dcbbd389a6ce9c75f60
+  log.Fatal(http.ListenAndServe(monitorPort, router))
 }
--- proto/GenerateGoCode.sh
+++ proto/GenerateGoCode.sh
@@ -3,3 +3,9 @@
 protoc --go_out=. --go_opt=paths=source_relative \
     --go-grpc_out=. --go-grpc_opt=paths=source_relative \
     govidia.proto
+
+# The following is a fix for protobuffer omiting fields with
+# zero/null values. Incidentally, this means GPUs with index of '0' 
+# get their gpu_index field cut off (oh fun!)
+ls *.pb.go | xargs -n1 -IX bash -c \
+"sed -e '/int32/ s/,omitempty//' X > X.tmp && mv X{.tmp,}"
